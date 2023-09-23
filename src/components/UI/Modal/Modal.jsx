e9ce2a28b9a6db264020971c75aaf4777ee658ea
Johnkennedy <muhumuzakennedy17@gmail.com>
2023-09-23 19:21:16 +0300

    remove portal from modal

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -32,7 +32,8 @@ const CLOSE_STYLES = {
 export default function Modal({ open, children, onClose }) {
   if (!open) return null;
 
-  return ReactDom.createPortal(
+  return (
+    //ReactDom.createPortal(
     <>
       {/* <div style={OVERLAY_STYLES}> */}
       <div className={styles["modal_overlay"]} />
@@ -44,7 +45,7 @@ export default function Modal({ open, children, onClose }) {
         </button>
         {children}
       </div>
-    </>,
-    document.getElementById("portal")
+    </> //,
+    //document.getElementById("portal")
   );
 }
